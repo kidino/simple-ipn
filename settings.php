a4d83d14138630b80ba5c9b88bae04c24a1f78c9
Iszuddin Ismail <dino@websegera.my>
2016-07-10 22:52:36 +0800

    update settings with default value
    Signed-off-by: Iszuddin Ismail <dino@websegera.my>

--- settings.php
+++ settings.php
@@ -6,9 +6,9 @@ $debug = 1;  // to enable debug mode. debug mode will dump IPN data and transact
              
 $paypal_sandbox = 1;  // 1 for test more, 0 for production mode
 $expire_in_hours = 72;  // how many hours before download expires, in hours
-$paypal_email_address = 'business@kengkawan.com';  // your paypal email address
-$support_email_address = 'admin@jomweb.my';   // your email address for support
-$support_email_name = 'SIPN';   // name to appear in the From when email is sent
+$paypal_email_address = 'paypal@domain.com';  // your paypal email address
+$support_email_address = 'support@domain.com';   // your email address for support
+$support_email_name = 'My Website';   // name to appear in the From when email is sent
 
 $get_shipping_address = 1;   // whether to get or not shipping address
                              // 1 - Yes, MUST enter shipping address
